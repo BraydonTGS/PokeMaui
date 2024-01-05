233955361a99f9a49214553ebbdd061f8cf9e13b
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-01-05 17:06:31 -0600

    Added First Migration, Created Database, Created DbContext Diagram

---
+++ PokeMaui.Business/Migrations/20240105230135_FirstMigration.cs
@@ -0,0 +1,241 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeMaui.Business.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ability",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AbilityName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ability", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Move",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MoveName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Move", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pokemon",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PokemonName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    BaseExperience = table.Column<int>(type: "int", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemon", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Type",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Type", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Form",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FormName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PokemonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Form", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Form_Pokemon_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemon",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonAbility",
+                columns: table => new
+                {
+                    PokemonId = table.Column<int>(type: "int", nullable: false),
+                    AbilityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonAbility", x => new { x.PokemonId, x.AbilityId });
+                    table.ForeignKey(
+                        name: "FK_PokemonAbility_Ability_AbilityId",
+                        column: x => x.AbilityId,
+                        principalTable: "Ability",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonAbility_Pokemon_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemon",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonMoves",
+                columns: table => new
+                {
+                    PokemonId = table.Column<int>(type: "int", nullable: false),
+                    MoveId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonMoves", x => new { x.PokemonId, x.MoveId });
+                    table.ForeignKey(
+                        name: "FK_PokemonMoves_Move_MoveId",
+                        column: x => x.MoveId,
+                        principalTable: "Move",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonMoves_Pokemon_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemon",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sprite",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PokemonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sprite", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sprite_Pokemon_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemon",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonTypes",
+                columns: table => new
+                {
+                    PokemonId = table.Column<int>(type: "int", nullable: false),
+                    TypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonTypes", x => new { x.PokemonId, x.TypeId });
+                    table.ForeignKey(
+                        name: "FK_PokemonTypes_Pokemon_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemon",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonTypes_Type_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "Type",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Form_PokemonId",
+                table: "Form",
+                column: "PokemonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonAbility_AbilityId",
+                table: "PokemonAbility",
+                column: "AbilityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonMoves_MoveId",
+                table: "PokemonMoves",
+                column: "MoveId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonTypes_TypeId",
+                table: "PokemonTypes",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sprite_PokemonId",
+                table: "Sprite",
+                column: "PokemonId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Form");
+
+            migrationBuilder.DropTable(
+                name: "PokemonAbility");
+
+            migrationBuilder.DropTable(
+                name: "PokemonMoves");
+
+            migrationBuilder.DropTable(
+                name: "PokemonTypes");
+
+            migrationBuilder.DropTable(
+                name: "Sprite");
+
+            migrationBuilder.DropTable(
+                name: "Ability");
+
+            migrationBuilder.DropTable(
+                name: "Move");
+
+            migrationBuilder.DropTable(
+                name: "Type");
+
+            migrationBuilder.DropTable(
+                name: "Pokemon");
+        }
+    }
+}
