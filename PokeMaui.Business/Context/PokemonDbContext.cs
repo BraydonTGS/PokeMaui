c5f2d9dd3eddf41febce05d958f2599721c01212
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-01-05 16:59:40 -0600

    Added DbContext and Set up Connection

--- PokeMaui.Business/Context/PokemonDbContext.cs
+++ PokeMaui.Business/Context/PokemonDbContext.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PokeMaui.Business.Connection;
+using PokeMaui.Entity.Entities;
 
 namespace PokeMaui.Business.Context
 {
-    public class PokemonDbContext
+    public class PokemonDbContext : DbContext
     {
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<Ability> Abilities { get; set; }
+        public DbSet<Form> Forms { get; set; }
+        public DbSet<Move> Moves { get; set; }
+        public DbSet<Sprite> Sprites { get; set; }
+        public DbSet<Entity.Entities.Type> Types { get; set; }
+        public DbSet<PokemonAbility> PokemonAbilities { get; set; }
+        public DbSet<PokemonMoves> PokemonMoves { get; set; }
+        public DbSet<PokemonTypes> PokemonTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder
+                    .UseSqlServer(Hidden.GetConnectionString())
+                    .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
+        }
+
+        // Configure Fluent API //
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
